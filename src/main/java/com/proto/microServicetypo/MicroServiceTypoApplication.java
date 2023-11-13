ca564cf2a9c2e759a949799869ec350cb107266f
a.turki <at.aslem@gmail.com>
2023-11-13 09:25:32 +0100

    V3 config Eureka client

---
+++ src/main/java/com/proto/microServicetypo/MicroServiceTypoApplication.java
@@ -0,0 +1,13 @@
+package com.proto.microServicetypo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceTypoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceTypoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proto/microServicetypo/MicroServiceTypoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proto.microServicetypo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServiceTypoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
